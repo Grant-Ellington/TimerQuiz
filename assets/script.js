dc9afeae4bbdb3942feeb2b46dee24f04f7674d8
Grant-Ellington <gellingtonem6@gmail.com>
2022-08-26 18:07:32 -0500

    created question spot, and for loop to create choices-need to add population iof questions

--- assets/script.js
+++ assets/script.js
@@ -3,33 +3,34 @@
 var questions = [
 
     {
-        question: "This is quetion 1?",
-        qanswerSelection: ['a answer', 'b answer', 'c answer', 'd answer'],
+        problem: ["This is quetion 1?"],
+        answerSelection: ['a answer', 'b answer', 'c answer', 'd answer'],
         answer: 'a answer'
     },
 
     {
-        question: "This is quetion 1?",
-        qanswerSelection: ['a answer', 'b answer', 'c answer', 'd answer'],
+        problem: "This is quetion 1?",
+        answerSelection: ['a answer', 'b answer', 'c answer', 'd answer'],
         answer: 'a answer'
     },
     
     {
-        question: "This is quetion 1?",
-        qanswerSelection: ['a answer', 'b answer', 'c answer', 'd answer'],
+        problem: "This is quetion 1?",
+        answerSelection: ['a answer', 'b answer', 'c answer', 'd answer'],
         answer: 'a answer'
     },
 
-
 ]
+
 
 //define variables for div spots via getElementById
 
 var showTime = document.getElementById('spot-1')
 var showScore = document.getElementById('spot-2')
 var button = document.getElementById('button')
-
-
+var showQuestion = document.getElementById('spot-3')
+var questionNum = 0 //This is to give the array vallue of the questions
+var choices = document.getElementById('spot-4')
 
 //Function to start timer
 var timeLeft = 10
@@ -48,10 +49,27 @@ function startTimer () {
 
         }
     },1000)
-    
-   
-    
+      
 };
+
+function displayQuestion() {
+    showQuestion.innerHTML = questions[questionNum].problem[0]//This is the order to target questions
+
+    //Create for loop to go through questions
+for ( i =0; i<=questions.length; i++) {
+    choices = document.creteElement('button')
+    choices.innerText = questions[questionNum].answerSelection[i]
+    selection.addEventListner('click', function (event) {
+        event.stopPropagation
+
+    if(selection.innerText === questions[questionNum].answer){
+        questionNum++;
+    }else {
+        console.log('WRONG ANSWER')
+    }
+    })};
+};
+
 
 
 
@@ -64,6 +82,7 @@ function startTimer () {
 //Button functionalites
 
 button.addEventListener('click', startTimer)
+button.addEventListener('click', displayQuestion)
 
 
 
